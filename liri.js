9ce906bbfb1782a277feab74ec9acd4a3b2d09a9
Richard Paez <Richardp809@gmail.com>
2018-03-08 10:56:25 -0500

    added the my-tweets functionality

--- liri.js
+++ liri.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const keys = require("./keys");
-//const spotify = new Spotify(keys.spotify);
-//const twitterClient = new Twitter(keys.twitter);
+const Twitter = require("twitter");
+const Spotify = require("node-spotify-api");
+const spotifyClient = new Spotify(keys.spotify);
+const twitterClient = new Twitter(keys.twitter);
 
 liri = function( command ){
     switch( command ){
         case "my-tweets":
             console.log("Here are my tweets!");
+            myTweets();
         break;
         case "spotify-this-song":
             console.log("Spotifying...");
@@ -21,6 +24,15 @@ liri = function( command ){
             console.log("Sorry, I didn't hear you.");
             break;
     }
+}
+
+function myTweets(){
+    twitterClient.get('statuses/home_timeline', function(error, tweets, response) {
+        if(error) throw error;
+        for( var i = 0; i < tweets.length; i++ ){
+            console.log(`Tweet #${(i+1)} | date: ${tweets[i].created_at} "${tweets[i].text}"`);
+        }
+    });
 }
 
 module.exports = liri;
